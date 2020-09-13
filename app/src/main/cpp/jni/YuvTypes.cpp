ee02b593b29f407cda62787379a803dd2729d047
hetao01 <hetao01@163.com>
2020-09-13 15:12:06 +0800

    修改kotlin版本

--- app/src/main/cpp/jni/YuvTypes.cpp
+++ app/src/main/cpp/jni/YuvTypes.cpp
@@ -5,11 +5,11 @@
 #include "libyuv.h"
 #include <jni.h>
 
-#include <s>
 // NV21转I420
 extern "C"
 JNIEXPORT void JNICALL
 Java_tt_reducto_ndksample_jni_YUVOps_NV21ToI420(JNIEnv *env, jobject thiz, jbyteArray input,jbyteArray output, jint width, jint height) {
 
     //
+
 }
